7de06199390165bf2c0675cd7fd007d54e148177
you21979 <you2197901@gmail.com>
2013-11-13 00:13:54 +0900

    add

---
+++ node/hash/wordcount2.js
@@ -0,0 +1,40 @@
+var redis = require('redis');
+
+var key = 'jobmap';
+var Chara = function(lv, job){
+    this.lv = lv;
+    this.job = job;
+}
+
+var job = [
+    'redmag',
+    'blackmag',
+    'whitemag',
+    'fighter',
+    'monk'
+];
+
+var u = function(i){
+    return new Chara( i, job[i % job.length] );
+}
+var lists = [];
+for(var i=0; i<10001; ++i) lists.push(u(Math.random() * 100 | 0));
+
+var cl = redis.createClient();
+
+cl.del(key);
+
+var m = cl.multi();
+
+lists.forEach(function(o){
+    m.hincrby(key, o.job + '.' + (o.lv / 10|0) + '0', 1);
+});
+m.hgetall(key, function(err, val){
+    console.log(val);
+});
+m.exec(function(err, result){
+    console.log('done');
+});
+cl.quit();
+
+
