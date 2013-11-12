f7103b6fa6c96872531a0b6c938d8fe7668e39b6
you21979 <you2197901@gmail.com>
2013-11-12 16:07:11 +0900

    add node

---
+++ node/sortedset/ranking.js
@@ -0,0 +1,32 @@
+var redis = require('redis');
+
+var member = [
+    'user1',
+    'user2',
+    'user3',
+    'user4',
+    'user5',
+    'user6',
+    'user7',
+    'user8'
+];
+var key = 'hogerank';
+var r = function(){
+    var i = Math.random() * member.length|0;
+    return member[i]
+}
+var cl = redis.createClient();
+
+cl.del(key);
+
+var m = cl.multi();
+for(var i=0;i<1000;++i){
+    m.zincrby(key, Math.random() * 100|0, r());
+}
+m.zrange(key, 0, member.length, function(err, val){
+    console.log(val);
+});
+m.exec(function(){
+    console.log('done');
+});
+cl.quit();
