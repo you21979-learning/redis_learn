dab54c278eadbbbf417d6f528ca813571b3a48ed
you21979 <you2197901@gmail.com>
2013-11-13 00:09:00 +0900

    mod

--- node/hash/wordcount.js
+++ node/hash/wordcount.js
@@ -1,6 +1,10 @@
 var redis = require('redis');
 
-var key = 'hogehash';
+var key = 'jobmap';
+var Chara = function(lv, job){
+    this.lv = lv;
+    this.job = job;
+}
 
 var job = [
     'redmag',
@@ -10,49 +14,26 @@ var job = [
     'monk'
 ];
 
-var n = 0;
 var u = function(i){
-    return job[i % job.length];
+    return new Chara( i, job[i % job.length] );
 }
-var lists = [
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++),
-    u(n++)
-];
+var lists = [];
+for(var i=0; i<10001; ++i) lists.push(u(i));
 
 var cl = redis.createClient();
 
 cl.del(key);
 
+var m = cl.multi();
+
 lists.forEach(function(o){
-    cl.hincrby(key, o, 1);
+    m.hincrby(key, o.job, 1);
 });
-cl.hgetall(key, function(err, val){
+m.hgetall(key, function(err, val){
     console.log(val);
+});
+m.exec(function(err, result){
+    console.log('done');
 });
 cl.quit();
 
